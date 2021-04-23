6a7a08d94b1e6d9f673033ade4ec6ff39cf8c579
Nhan Vu <nhan997@gmail.com>
2021-04-23 19:30:00 +0200

    predict model

--- app.py
+++ app.py
@@ -1,11 +1,25 @@
-from flask import Flask
+from flask import Flask, request, jsonify
+from flask_restful import Resource, Api, reqparse
+from model import Model
 
 app = Flask(__name__)
+model = Model('C:\\Users\\TrungNhan.Vu\\OneDrive - insidemedia.net\\Desktop\\Bachelorarbeit\\Model')
+model.create_model()
 
 
-@app.route('/')
-def hello_world():
-    return 'Hello World!'
+@app.route("/")
+def index():
+    return 'NLP Service is running'
+
+
+@app.route("/api/predict", methods=['POST'])
+def prediction():
+    content = request.json
+    sentence1 = content['sentence1']
+    sentence2 = content['sentence2']
+    prediction_result = model.check_similarity(sentence1, sentence2)
+    result = str(prediction_result['True'])
+    return jsonify(result)
 
 
 if __name__ == '__main__':
---
+++ model.py
@@ -0,0 +1,68 @@
+import tensorflow as tf
+import numpy as np
+import transformers
+from data_generator import BertSemanticDataGenerator
+
+
+class Model:
+    def __init__(self, path):
+        self.path = path
+        self.model = None
+
+    def create_model(self):
+        # Create the model under a distribution strategy scope.
+        strategy = tf.distribute.MirroredStrategy()
+
+        with strategy.scope():
+            # Encoded token ids from BERT tokenizer.
+            input_ids = tf.keras.layers.Input(
+                shape=(128,), dtype=tf.int32, name="input_ids"
+            )
+            # Attention masks indicates to the model which tokens should be attended to.
+            attention_masks = tf.keras.layers.Input(
+                shape=(128,), dtype=tf.int32, name="attention_masks"
+            )
+            # Token type ids are binary masks identifying different sequences in the model.
+            token_type_ids = tf.keras.layers.Input(
+                shape=(128,), dtype=tf.int32, name="token_type_ids"
+            )
+            # Loading pretrained BERT model.
+            bert_model = transformers.TFBertModel.from_pretrained("bert-base-uncased")
+            # Freeze the BERT model to reuse the pretrained features without modifying them.
+            bert_model.trainable = False
+
+            bert_outputs = bert_model(
+                input_ids, attention_mask=attention_masks, token_type_ids=token_type_ids
+            )
+            last_hidden_state = bert_outputs[0]
+
+            # Add trainable layers on top of frozen layers to adapt the pretrained features on the new data.
+            bi_lstm = tf.keras.layers.Bidirectional(
+                tf.keras.layers.LSTM(64, return_sequences=True)
+            )(last_hidden_state)
+            # Applying hybrid pooling approach to bi_lstm sequence output.
+            avg_pool = tf.keras.layers.GlobalAveragePooling1D()(bi_lstm)
+            max_pool = tf.keras.layers.GlobalMaxPooling1D()(bi_lstm)
+            concat = tf.keras.layers.concatenate([avg_pool, max_pool])
+            dropout = tf.keras.layers.Dropout(0.3)(concat)
+            output = tf.keras.layers.Dense(2, activation="softmax")(dropout)
+            self.model = tf.keras.models.Model(
+                inputs=[input_ids, attention_masks, token_type_ids], outputs=output
+            )
+
+            self.model.compile(
+                optimizer=tf.keras.optimizers.Adam(),
+                loss="categorical_crossentropy",
+                metrics=["acc"],
+            )
+
+            self.model.load_weights(self.path + '/fine-tuned-model.h5')
+
+    def check_similarity(self, sentence1, sentence2):
+        sentence_pairs = np.array([[str(sentence1), str(sentence2)]])
+        test_data = BertSemanticDataGenerator(
+            sentence_pairs, labels=None, batch_size=1, shuffle=False, include_targets=False,
+        )
+
+        probability = self.model.predict(test_data)[0]
+        return {'False': probability[0], 'True': probability[1]}
