aca1b7b5cf9a5e64cae5ebac590a72cef4a4993b
Nhan Vu <nhan997@gmail.com>
2021-04-23 19:52:40 +0200

    add fine-tuned model

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ from flask_restful import Resource, Api, reqparse
 from model import Model
 
 app = Flask(__name__)
-model = Model('C:\\Users\\TrungNhan.Vu\\OneDrive - insidemedia.net\\Desktop\\Bachelorarbeit\\Model')
+model = Model('./nlp-models/fine-tuned-model.h5')
 model.create_model()
 
 
